ac98c2c551574a762c85666baedd62d99c2eb84b
festring <hch2454@khu.ac.kr>
2023-11-18 17:25:26 +0900

    여러 계정 크롤링

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 async function show_review(page) {
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(2000);
   await page.evaluate(() => {
     // 페이지의 맨 아래로 스크롤
     window.scrollTo({
@@ -11,13 +11,13 @@ async function show_review(page) {
   // 1초 대기
   await page.waitForTimeout(2000);
   // 페이지의 맨 위로 스크롤
-  await page.evaluate(() => {
+  // await page.evaluate(() => {
 
-    window.scrollTo({
-      top: 0,
-      behavior: 'auto'
-    });
-  });
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'auto'
+  //   });
+  // });
 }
 
 async function waitForSeconds(time) {
@@ -34,10 +34,12 @@ async function getMemberIds(page) {
 
 async function count_scroll(page) {
   return await page.evaluate(async () => {
-    const totalCountElement = document.querySelector('span.js_reviewProfileModalListTotalCount');
-    const totalCountText = totalCountElement.textContent;
-    const totalCount = parseInt(totalCountText.replace(/,/g, ''), 10);
-    const result = Math.ceil(totalCount / 15);
+    // const totalCountElement = document.querySelector('span.js_reviewProfileModalListTotalCount');
+    // console.log(totalCountElement);
+    // const totalCountText = totalCountElement.textContent;
+    // const totalCount = parseInt(totalCountText.replace(/,/g, ''), 10);
+    //console.log(totalCount);
+    const result = 20;
     return result;  
   });
 }
@@ -59,8 +61,8 @@ async function data(page,id_get){
     const productNames = document.querySelectorAll('.sdp-review__profile__article__list__reviews__product__name');
     const starDates = document.querySelectorAll('.sdp-review__profile__article__list__reviews__star__date');
     const sellerNames = document.querySelectorAll('.sdp-review__profile__article__list__reviews__seller_name');
-    const contentExistence = document.querySelectorAll('.sdp-review__profile__article__list__reviews__title');
-    
+    const reviewContentElement = document.querySelectorAll('.sdp-review__profile__article__list__reviews__content');
+
     const id_list = [];
     
     id_list.push(id);
@@ -71,12 +73,17 @@ async function data(page,id_get){
       const starDate = starDates[index].textContent.trim();
       const sellerName = sellerNames[index].textContent.trim();
       //const hasContent = contentExistence[index] ? 1 : 0;
-      const commentLength = contentExistence[index] ? contentExistence[index].textContent.length : 0;
+      // 텍스트 내용 가져오기
+      const textContent = reviewContentElement[index].textContent;
+
+      // HTML 태그 및 공백 제외한 글자 수 계산
+      const characterCount = textContent.length;
+      
       item.push(productName);
       item.push(starDate);
       item.push(sellerName);
       //item.push(hasContent);
-      item.push(commentLength);
+      item.push(characterCount);
       id_list.push(item);
     });
     return id_list;
@@ -84,16 +91,16 @@ async function data(page,id_get){
 }
 
 
-async function test(page,i, id_list ){
-  await page.evaluate(async (i, id_list ) => {
+async function test(page,id ){
+  await page.evaluate(async (id) => {
     const elements = await document.querySelectorAll('.sdp-review__article__list__info__user__name.js_reviewUserProfileImage');
-    await elements[i].click();
-    await new Promise(resolve => setTimeout(resolve, 300));
-  },i, id_list);
+    elements[0].setAttribute('data-member-id', id);
+    await elements[0].click();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  },id);
   const count_page = await count_scroll(page);
-  await new Promise(resolve => setTimeout(resolve, 300));
   await scroll(page,count_page);
-  human_list= await data(page,id_list[i]);
+  human_list= await data(page,id);
   return human_list;
 }
 
@@ -104,8 +111,8 @@ async function test(page,i, id_list ){
 //푸피터 열기
 const puppeteer = require('puppeteer');
 
-//푸피터 열기
-async function final() {
+//푸피터 실행
+async function final(id) {
   const browser = await puppeteer.launch({
     headless: false
     ,args: ['--blink-settings=imagesEnabled=false'] // 이미지 로딩 비활성화
@@ -113,26 +120,47 @@ async function final() {
   // 페이지 열기
   const page = await browser.newPage();
   await page.goto('https://www.coupang.com/vp/products/7345174742?itemId=18888453974&vendorItemId=86016622490&sourceType=cmgoms&omsPageId=103048&omsPageUrl=103048&isAddedCart=');
-  //결과 미리 선언
-  page_db=[];
+  
   // 리뷰 소환
   await show_review(page);
-  // 아이디 리스트 생성
-  const id_list = await getMemberIds(page);
-
-
-  const human_list= await test(page,0,id_list);
+  // 크롤링 진행
+  const human_list= await test(page,id);
 
   //결과 테스트
-  await console.log('Member IDs:', id_list);
-  await console.log("human_list:",human_list);
-
-  
+  //await console.log("human_list:",human_list);  
   
   // 브라우저 닫기
   //await browser.close();
+  return human_list;
 }
 
 // 함수 호출
-final();
+async function main(id_get) {
+  page_db = [];
 
+  const promises = id_get.map(id => final(id));
+
+  try {
+    const results = await Promise.all(promises);
+    page_db.push(...results);
+    console.log(page_db);
+    //return page_db;
+  } catch (error) {
+    console.error('하나 이상의 Promise에서 에러 발생:', error);
+  }
+}
+
+//////////////////////////////////////////////test
+
+// async function main(id_get) {
+//   final(id_get);
+// }
+
+id_five= ['11330282', '3419688', '13538132', '3156260', '123366059'];
+id_two= ['11330282', '3419688'];
+// result =main(id_five[0]);
+
+//////////////////////////////////////////////test
+
+result =main(id_five);
+//console.log(result);  
